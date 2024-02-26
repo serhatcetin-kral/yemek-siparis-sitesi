7bda761530224c739e9a07c47d633b2ae93ad926
serhatcetin-kral <serhat06.sc@gmail.com>
2024-02-26 09:28:31 -0500

    after order page

--- backend/src/routers/order.router.js
+++ backend/src/routers/order.router.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import handler from 'express-async-handler';
 import auth from '../middleware/auth.mid.js';
-import { BAD_REQUEST } from '../constants/httpStatus.js';
+import { BAD_REQUEST, UNAUTHORIZED } from '../constants/httpStatus.js';
 import { OrderModel } from '../models/order.model.js';
 import { OrderStatus } from '../constants/orderStatus.js';
+import { UserModel } from '../models/user.model.js';
 
 
 
@@ -52,10 +53,24 @@ router.post(
   );
 
  
-// router.get('/track/:orderId',handler(async (req,res)=>{
-// const {or}
-// })
-// );
+router.get('/track/:orderId',
+handler(async (req,res)=>{
+const {orderId}=req.params;
+const user=await UserModel.findById(req.user.id);
+
+const filter={
+  _id:orderId,
+};
+
+if(!user.isAdmin){
+  filter.user=user._id;
+}
+const order=await OrderModel.findOne(filter);
+if(!order) return res.send(UNAUTHORIZED);
+return res.send(order);
+
+})
+);
 
   router.get(
     '/newOrderForCurrentUser',
@@ -66,6 +81,28 @@ router.post(
       else res.status(BAD_REQUEST).send();
     })
   );
+
+
+  router.get('/allstatus', (req, res) => {
+    const allStatus = Object.values(OrderStatus);
+    res.send(allStatus);
+  });
+  
+  router.get(
+    '/:status?',
+    handler(async (req, res) => {
+      const status = req.params.status;
+      const user = await UserModel.findById(req.user.id);
+      const filter = {};
+  
+      if (!user.isAdmin) filter.user = user._id;
+      if (status) filter.status = status;
+  
+      const orders = await OrderModel.find(filter).sort('-createdAt');
+      res.send(orders);
+    })
+  );
+  
 
 const getNewOrderForCurrentUser=async req=>
 await OrderModel.findOne({user:req.user.id,status:OrderStatus.NEW});
