eedfdded919ca58c04c3ffb61889fa2e88c6a521
serhatcetin-kral <serhat06.sc@gmail.com>
2024-03-19 15:07:33 -0400

    renderpush

--- backend/src/server.js
+++ backend/src/server.js
@@ -38,7 +38,7 @@ app.get('*', (req, res) => {
 });
 
 
-const PORT=5000;
-app.listen(5000 ,()=>{ 
+const PORT=process.env.PORT || 5000;
+app.listen(PORT ,()=>{ 
     console.log('listening  on port '+ PORT);
 });
